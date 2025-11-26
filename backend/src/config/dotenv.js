4838648d2f150b8b744c8c3a1df0dffea0459f4d
Arvindh Shankar <161598154+AravindhshankarTansam@users.noreply.github.com>
2025-11-26 16:38:48 +0530

    env file changed

--- backend/src/config/dotenv.js
+++ backend/src/config/dotenv.js
@@ -4,7 +4,7 @@ import path from "path";
 import fs from "fs";
 
 // Detect environment (default = development)
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "production ";
 
 // Select correct .env file
 const envFile = env === "production" ? ".env.production" : ".env.local";
